f546988098420cc31a3e005739c479b17b572960
christyuda <chrisyuda@ulbi.ac.id>
2024-05-21 00:05:09 +0700

    chore: Add itty-router and realm-web dependencies, update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,64 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// };
+
+import { Router } from 'itty-router';
+import * as Realm from 'realm-web';
+
+const router = Router();
+
+// Replace 'application-0-ecvmjju' with your actual MongoDB App ID
+const app = new Realm.App({ id: 'application-0-ecvmjju' });
+
+const getBenchmarks = async (request: Request): Promise<Response> => {
+	try {
+	  const apiKey = request.headers.get('Authorization');
+	  if (!apiKey) return new Response('Missing API Key', { status: 401 });
+
+	  const credentials = Realm.Credentials.apiKey(apiKey);
+	  const user = await app.logIn(credentials);
+	  const mongodb = user.mongoClient('mongodb-atlas');
+	  const benchmarksCollection = mongodb.db('cloudflare').collection('benchmarks');
+
+	  const benchmarks = await benchmarksCollection.find();
+	  return new Response(JSON.stringify(benchmarks), { status: 200 });
+	} catch (error) {
+	  return new Response(`Error: ${(error as Error).message}`, { status: 500 });
+	}
+  };
+
+  const postBenchmark = async (request: Request): Promise<Response> => {
+	try {
+	const apiKey = request.headers.get('Authorization');
+	if (!apiKey) return new Response('Missing API Key', { status: 401 });
+
+	const credentials = Realm.Credentials.apiKey(apiKey);
+	const user = await app.logIn(credentials);
+	const mongodb = user.mongoClient('mongodb-atlas');
+	const benchmarksCollection = mongodb.db('cloudflare').collection('benchmarks');
+
+	const benchmark = await request.json();
+	const result = await benchmarksCollection.insertOne({
+		owner_id: user.id,
+		benchmark,
+	});
+
+	  return new Response(JSON.stringify(result), { status: 201 });
+	} catch (error) {
+		return new Response(`Error: ${(error as Error).message}`, { status: 500 });
+	}
+  };
+
+  // Define the Worker logic
+  router.get('/benchmarks', getBenchmarks);
+  router.post('/benchmarks', postBenchmark);
+
+  // Event listener for handling requests
+  addEventListener('fetch', (event) => {
+	event.respondWith(router.handle(event.request));
+  });
